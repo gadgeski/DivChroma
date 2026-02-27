1998785f584734b4a954aea44f7807030e7def54
gadgeski <xtwh.ce5bil@gmail.com>
2026-02-27 23:43:18 +0900

    fix: correct package name in ExampleInstrumentedTest

--- app/src/androidTest/java/com/gadgeski/divchroma/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/gadgeski/divchroma/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.divchroma", appContext.packageName)
+        assertEquals("com.gadgeski.divchroma", appContext.packageName)
     }
 }
